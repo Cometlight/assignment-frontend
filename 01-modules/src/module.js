a027e96f1870156fba2209c85892036127cbfe46
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2016-11-10 00:23:07 +0100

    Add email analyzer functions + additional tests

--- 01-modules/src/module.js
+++ 01-modules/src/module.js
@@ -1,15 +1,74 @@
+/**
+ * @file Provides functions for analyzing email adresses.
+ * @author Daniel Scheffknecht
+ */
+
+const regexEmail = /^[a-z]{2,}\.([a-z]{3})-([b|m])(\d{4})@fh-salzburg\.ac\.at$/i;
+const regexEmailGroups = {
+  DegreeProgram: 1,
+  Level: 2,
+  StartingYear: 3
+};
+const DURATION_BACHELOR_DEGREE = 3; // [Years]
+const DURATION_MASTER_DEGREE = 2 // [Years]
+
+/**
+ * Determines if the given email adress is a valid FHS email adress.
+ * Note that email adresses of all degree courses (mmt, mma, ..) are
+ * seen as valid.
+ * 
+ * @param {string} email
+ * @returns {Boolean}
+ */
 export function valid(email) {
-
+  var isValid = regexEmail.test(email);
+  return isValid;
 }
 
+/**
+ * Extracts the degree program (MMT, MMA, ..) from the email adress.
+ * 
+ * @param {string} email
+ * @returns {string|Boolean} The degree program as string, or false if it could not be extracted.
+ */
 export function degreeProgram(email) {
-
+  var groups = regexEmail.exec(email);
+  if (groups) {
+    return groups[regexEmailGroups.DegreeProgram].toUpperCase();
+  } else {
+    return false;
+  }
 }
 
+/**
+ * Extracts the level (BA or MA) from the email adress.
+ * 
+ * @param {string} email
+ * @returns {string|Boolean} The level as string, or false if it could not be extracted.
+ */
 export function level(email) {
-
+  var groups = regexEmail.exec(email);
+  if (groups) {
+    var level = groups[regexEmailGroups.Level].toUpperCase() + 'A';
+    return level;
+  } else {
+    return false;
+  }
 }
 
+/**
+ * Returns the graduation year based on the given email adress.
+ * 
+ * @param {string} email
+ * @returns {Number|Boolean} The graduation year as number, or false if it could not be extracted.
+ */
 export function graduationYear(email) {
+  var groups = regexEmail.exec(email);
+  if (!groups)
+    return false;
 
+  var startingYear = groups[regexEmailGroups.StartingYear];
+  var level = groups[regexEmailGroups.Level];
+  var graduationYear = parseInt(startingYear, 10) + ( level === 'b' ? DURATION_BACHELOR_DEGREE : DURATION_MASTER_DEGREE );
+  return graduationYear;
 }
--- 01-modules/test/index.js
+++ 01-modules/test/index.js
@@ -8,29 +8,49 @@ import {
 
 describe('01-modules', () => {
   describe('#valid()', () => {
-    it.skip('should be a valid FH/MMT email', () => {
-      valid('hmoser.mmt-b2015@fh-salzburg.ac.at').should.be.true()
+    it('should be a valid FH email', () => {
+      valid('hmoser.mmt-b2015@fh-salzburg.ac.at').should.be.true();
+      valid('hmoser.mmt-m2016@fh-salzburg.ac.at').should.be.true();
+      valid('HMOSER.mmt-m2016@fh-salzburg.ac.at').should.be.true();
+      valid('ds.mmt-b2014@fh-salzburg.ac.at').should.be.true();
+      valid('hmoser.mma-b2015@fh-salzburg.ac.at').should.be.true();
     })
-    it.skip('should be an invalid FH/MMT email', () => {
+    it('should be an invalid FH email', () => {
       valid('hannes.moser@fh-salzburg.ac.at').should.be.false()
+      valid('h.mmt-b2015@fh-salzburg.ac.at').should.be.false()
+      valid('hmoser.mmt-a2015@fh-salzburg.ac.at').should.be.false()
+      valid('hmoser.mmt-b201@fh-salzburg.ac.at').should.be.false()
+      valid('hmoserxmmt-b2015@fh-salzburg.ac.at').should.be.false()
+      valid('hmoser.mmt-b2015@fh-salzburgxac.at').should.be.false()
+      valid('hmoser.mmt-b2015@fh-salzburg.acxat').should.be.false()
+      valid('hmoser.mmt-b2015fh-salzburg.ac.at').should.be.false()
+      valid('hmoser.mmtb2015@fh-salzburg.ac.at').should.be.false()
+      valid('hmoser.a4b-b2015@fh-salzburg.ac.at').should.be.false()
+      valid('email: hmoser.mmt-b2015@fh-salzburg.ac.at').should.be.false()
+      valid('hmoser.mmt-b2015@fh-salzburg.ac.at is an email').should.be.false()
     })
   })
 
   describe('#degreeProgram()', () => {
-    it.skip('should match MMT', () => {
+    it('should match MMT', () => {
       degreeProgram('hmoser.mmt-b2015@fh-salzburg.ac.at').should.eql('MMT')
+    })
+    it('should match MMA', () => {
+      degreeProgram('hmoser.mma-b2015@fh-salzburg.ac.at').should.eql('MMA')
     })
   })
 
   describe('#level()', () => {
-    it.skip('should match BA or MA', () => {
+    it('should match BA or MA', () => {
       level('hmoser.mmt-b2015@fh-salzburg.ac.at').should.eql('BA')
+      level('hmoser.mmt-m2015@fh-salzburg.ac.at').should.eql('MA')
     })
   })
 
   describe('#graduationYear()', () => {
-    it.skip('should be your graduation year', () => {
+    it('should be your graduation year', () => {
       graduationYear('hmoser.mmt-b2015@fh-salzburg.ac.at').should.eql(2018)
+      graduationYear('hmoser.mmt-m2015@fh-salzburg.ac.at').should.eql(2017)
     })
   })
 })
