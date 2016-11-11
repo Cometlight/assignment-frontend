39346cdceea9ca2820cfe49ecba021cf9fdca74b
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2016-11-12 00:34:19 +0100

    Finalize router (task02)

--- 02-router/src/app.js
+++ 02-router/src/app.js
@@ -3,8 +3,10 @@ import router from './router'
 import homeTpl from './templates/home.hbs'
 import contactTpl from './templates/contact.hbs'
 import notFoundTpl from './templates/not-found.hbs'
+import magnusTpl from './templates/magnus.hbs'
+import sergeyTpl from './templates/sergey.hbs'
 
-const $app = $('#app')
+const $app = $('#app') // <main> .. contains the main content of the document
 
 function index() {
   $app.html(homeTpl())
@@ -18,7 +20,28 @@ function notFound() {
   $app.html(notFoundTpl())
 }
 
+function playerMagnus() {
+  $app.html(magnusTpl())
+}
+
+function playerSergey() {
+  $app.html(sergeyTpl())
+}
+
 router('/', index)
 router('/contact', contact)
+router('/players/:player', function(playerName) {
+  switch(playerName) {
+    case 'magnus':
+        playerMagnus();
+      break;
+    case 'sergey':
+        playerSergey();
+      break;
+    default:
+      console.log('Unknown player "' + playerName + '".')
+      notFound();
+  }
+});
 router('*', notFound)
 router()
--- 02-router/src/router.js
+++ 02-router/src/router.js
@@ -1,3 +1,108 @@
-export default function() {
+import $ from 'jquery'
 
+let routingTable = {}; // structure: routingTable[path]: {callback: theCallbackFunction, fullpath: theFullPath}
+
+export default function router(path, callback) {
+  if (path != undefined && callback != undefined) {
+    createNewEntryInRoutingTable(path, callback);
+  } else if (path != undefined && callback == undefined) {
+    routeTo(path)
+  } else /* path == undefined && callback == undefined */ {
+    // router initialized
+    router(getCurrentPathFromUrl());
+  }
+}
+
+$(document).ready(function() {
+  adjustRoutingLinks();
+  window.onpopstate = function(event) {
+    router(event.state.path);
+  }
+});
+
+function createNewEntryInRoutingTable(path, callback) {
+  const regexDynamicParam = /(.*\/)(:.*)/i; // e.g. /players/:player --> (/players)/(:player)
+  // const regexDynamicParam = /(.*)\/(:.*)/i; // e.g. /players/:player --> (/players)/(:player)
+  let regexResult = regexDynamicParam.exec(path);
+  if (regexResult !== null) {
+    // Path with dynamic parameter
+    let pathWithoutParameter = regexResult[1];
+    routingTable[pathWithoutParameter] = {
+      callback,
+      fullPath: path
+    }
+  } else {
+    // Static path
+    routingTable[path] = {
+      callback,
+      fullpath: path
+    }
+  }
+}
+
+function routeTo(path) {
+  setUrl(path);
+  // check if there are parameters -> dynamic routing
+  const regexDynamicParam = /(\/.*?\/)(.*)/i; // e.g. /players/daniel --> (/players)/(daniel)
+  let regexResult = regexDynamicParam.exec(path);
+  if (regexResult !== null) {
+    // We have a dynamic route with a parameter
+    let pathWithoutParameter = regexResult[1];
+    let paramValue = regexResult[2];
+    if (paramValue !== '' && routingTable.hasOwnProperty(pathWithoutParameter)) {
+      routingTable[pathWithoutParameter].callback(paramValue);
+    } else {
+      fallback();
+    }
+  } else if (routingTable.hasOwnProperty(path)) {
+    // We have a static router
+    routingTable[path].callback();
+  } else {
+    fallback();
+  }
+}
+
+function fallback() {
+  if (routingTable.hasOwnProperty('*')) {
+    // fallback if set
+    routingTable['*'].callback();
+  }
+}
+
+function getCurrentPathFromUrl() {
+  let regex = /[^\/]*\/\/[^\/]+(\/.*)/i;
+  let path = regex.exec(document.URL)[1];
+  return path;
+}
+
+
+/**
+ * @param {any} path e.g. /players/magnus --> http:://www.foo.com/players/magnus
+ */
+function setUrl(path) {
+  let regex = /^([^\/]*\/\/[^\/]+)\//i;
+  let interestingUrlPart = regex.exec(document.URL)[1];
+  let newUrl = interestingUrlPart + path;
+  // Only set it, if it's not the same already
+  if (document.URL !== newUrl) {
+    history.pushState({path}, path, newUrl);
+  }
+}
+
+function adjustRoutingLinks() {
+  // Select only the links which are used for routing
+  let filteredLinks = $('a[href]')
+    .not('a[download]')
+    .not('a[rel="external"]')
+    .not('a[rel="download"]')
+    .not('a[target="_blank"]')
+    .not('a[href^="//"]')
+    .not('a[href^="http"]');
+  filteredLinks.each(function() {
+    // $(this) is the current link
+    $(this).on('click', function(e) {
+      router($(this).attr('href'));
+      e.preventDefault();
+    });
+  });
 }
